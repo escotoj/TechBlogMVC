18bb6789bb4e453ea8724e44cf0a266ca41418d1
escotoj <j.escoto14@gmail.com>
2023-05-10 23:04:37 -0700

    worked on the routes and got them to render with handlebars, changes the file names in the views

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,9 +2,15 @@ const router = require('express').Router();
 const {User} = require('../../models');
 
 // here we will write the routes for dashboard, login and home.
-router.get('/users', (req, res) => {
-  res.render('users', {
-    sentence: 'this will be the users login?'
+router.get('/', (req, res) => {
+  res.render('welcome', {
+    sentence: 'this will be welcome page once signed in'
+  })
+})
+
+router.get('/signup', (req, res) => {
+  res.render('signup', {
+    sentence: 'maybe collect name or initials'
   })
 })
 
