e3d1a4926a972dcfbadfcef64104b4ee58ed5c19
escotoj <j.escoto14@gmail.com>
2023-05-11 09:58:40 -0700

    added public folder and css, but it is not being read

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 // MiddleWare - Express
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes)
