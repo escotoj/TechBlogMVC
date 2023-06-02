7a015f8e9bc546d2a40cf6bde22f6b5636b6d6a9
escotoj <j.escoto14@gmail.com>
2023-06-02 10:34:24 -0700

    added js function to the single blog page throuhg the homePage.js

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,48 +1,42 @@
 
-// const $editBtn = document.getElementById('editBtn');
-// const $deleteBtn = document.getElementById('deleteBtn');
+const $editBtn = document.getElementById('editBtn');
+const $deleteBtn = document.getElementById('deleteBtn');
 
-// $editBtn.addEventListener('click', async (event) => {
-//   console.log('click', event.target);
-//   document.location.replace('/dashboard');
-//   document.location.replace(`/dashboard/${id}`);
-//     try {
-//       const response = await fetch('/api/blog/dashboard', {
-//         method: 'PUT',
-//         body: JSON.stringify({newBlog}),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         }
-//       });
+$editBtn.addEventListener('click', async (event) => {
+  console.log('click', event.target);
+  // document.location.replace('/dashboard');
+  // document.location.replace(`/dashboard/${id}`);
+    try {
+      const response = await fetch(`/dashboard/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({newBlog}),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
       
-//       const data = await response.json();
-//       console.log(data);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   });
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 
+  $deleteBtn.addEventListener('click', async (event) => {
+    console.log('click')
+    try {
+    const id = event.target.getAttribute('data-id')
+      const response = await fetch(`/api/blog/dashboard/${id}`, {
+        method: 'DELETE',
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-
-
-
-  
-
-  // $deleteBtn.addEventListener('click', async (event) => {
-  //   console.log('click')
-  //   try {
-  //   const id = event.target.getAttribute('data-id')
-  //     const response = await fetch(`/api/blog/dashboard/${id}`, {
-  //       method: 'DELETE',
-  //     });
-  //     const data = await response.json();
-  //     console.log(data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // });
-
-  // $deleteBtn.addEventListener('click', async (event) => {
-  //   console.log("click", event)
-  // })
+  $deleteBtn.addEventListener('click', async (event) => {
+    console.log("click", event)
+  })
